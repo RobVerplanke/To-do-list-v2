d12b3f94cf2e5e413fac8007028dc4b702c1d562
RobVerplanke <rverplanke@gmaio.com>
2023-06-05 14:11:03 +0200

    geterate tasks dynamicly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Task from './modules/todo';
+import { printTasks } from './modules/ui';
 
 const newTask = new Task('Test to-do task', 'just for testing', 'note', false, 'date');
+printTasks();
 console.log(newTask);
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -2,3 +2,27 @@
 export default function uiTest() {
   console.log('ui.js log');
 }
+
+function printTasks() {
+  const mainContent = document.querySelector('#main-content');
+  const task = document.querySelector('.task');
+  task.innerHTML += `
+    <br><ul class="task">
+      <li class="task-checkbox">
+          <form action="">
+             <input type="checkbox" name="taskDelete" id="task-delete">
+          </form>
+       </li>
+       <li class="task-text">
+         <div class="task-title"><p>Schoonmaken</p></div>
+         <div class="task-description"><p>Slaapkamer opruimen, vegen/stofzuigen</p></div>
+       </li>
+       <li class="task-edit"><span class="material-symbols-outlined" title="Edit task">edit_note</span></li>
+       <li class="task-delete"><span class="material-symbols-outlined" title="Delete task">delete</span></li>
+    </ul>
+  `;
+
+  mainContent.append(task);
+}
+
+export { printTasks };
